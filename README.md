0d6a0c02546eebfa9832061d6c76d4804d3abbc5
Dmytro Nechepurenko <dimanechepurenko@gmail.com>
2019-07-23 00:32:24 +0300

    ADD: pagination

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Snap scroll carousel
 
-https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-snap-type
+It has been long ago that I was start thinking that classical carousel is absolutely redundant thing.
+It could be fully replaced by native scroll functionality - that would contribute to grater UX than this, old stuff button navigating only carousels.
+You probably know that scroll is very complex function that very hard to implement without access to native OS API. 
+That's why native scroll is more comfortable for the user (on modern desktop and all mobile) than any button-driven-functionality.
+
+## Snap scroll
+
+It has been 3 years of so when this API available for browsers. 
+It is browser adopted extension over native scroll API with what you could control scroll functionality from CSS.
+
+* MDN https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-snap-type
+* W3C v1 https://www.w3.org/TR/2015/WD-css-snappoints-1-20150326/
+* W3C v2 https://www.w3.org/TR/css-scroll-snap-1/
+* Caniuse https://caniuse.com/#feat=css-snappoints
+
+### Infinite scrolling
+
+I do not know why it marquee pattern even needed, especially for carousels, 
+that mostly represent huge content, that not permanent visible for the user.
+
+For that type of content it is more important to browse UX than "auto scrolling".
+
+## Browser support
+
+As progressive enhancement supports in every browsers from Netscape navigator 1.0 to latest Firefox.
+
+It is progressive enhancement component. 
+If no JS available it would be just native scroll. 
+If scroll snap CSS API available it would be snapped to declared values.
+If JS and snap scroll available than user will have Buttons and pagination functionality improvements over regular scroll.
+
+Snap scroll support https://caniuse.com/#feat=css-snappoints
+
+## License
+
+MIT D.Nechepurenko 2019
--- SnapScrollCarousel.js
+++ SnapScrollCarousel.js
@@ -4,6 +4,7 @@ class ScrollCarousel {
 		this.carouselTrack = this.carousel.querySelector('[data-carousel-track]');
 		this.prevButton = this.carousel.querySelector('[data-prev-button]');
 		this.nextButton = this.carousel.querySelector('[data-next-button]');
+		this.currentPage = 0;
 
 		this.itemsContainerWidth = this.carouselTrack.offsetWidth;
 		this.scrollEndSensitivity = 40;
@@ -11,7 +12,10 @@ class ScrollCarousel {
 
 	init() {
 		this.addEventListeners();
+		this.updateCarouselState();
 		this.carousel.classList.add('_inited');
+		this.createPagination();
+		this.setActivePagination();
 	}
 
 	addEventListeners() {
@@ -30,46 +34,95 @@ class ScrollCarousel {
 		this.scrollEnd = totalScrollWidth + this.scrollEndSensitivity >= this.carouselTrack.scrollWidth;
 
 		this.updateCarouselState();
+		this.setActivePagination();
 	}
 
 	updateCarouselState() {
+		if (this.carouselTrack.scrollWidth <= this.carouselTrack.clientWidth) {
+			this.prevButton.setAttribute('disabled', 'true');
+			this.nextButton.setAttribute('disabled', 'true');
+			this.carousel.classList.remove('_prev-visible');
+			this.carousel.classList.remove('_next-visible');
+			return;
+		}
+
 		if (this.scrollStart) {
 			this.carousel.classList.remove('_prev-visible');
+			this.prevButton.setAttribute('disabled', 'true');
 		} else {
 			this.carousel.classList.add('_prev-visible');
+			this.prevButton.removeAttribute('disabled');
 		}
 
 		if (this.scrollEnd) {
 			this.carousel.classList.remove('_next-visible');
+			this.nextButton.setAttribute('disabled', 'true');
 		} else {
 			this.carousel.classList.add('_next-visible');
+			this.nextButton.removeAttribute('disabled');
 		}
 	}
 
 	next() {
-		// scrollIntoView
+		this.scrollTo(this.carouselTrack.scrollLeft += this.carouselTrack.clientWidth)
+	}
+
+	prev() {
+		this.scrollTo(this.carouselTrack.scrollLeft -= this.carouselTrack.clientWidth)
+	}
+
+	scrollTo(left) {
 		// Safari old + Edge not smooth scroll
 		if (typeof this.carouselTrack.scrollTo === 'function') {
 			this.carouselTrack.scrollTo({
 				top: 0,
-				left: this.carouselTrack.scrollLeft += this.carouselTrack.clientWidth,
+				left: left,
 				behavior: 'smooth'
 			});
 		} else {
-			this.carouselTrack.scrollLeft += this.carouselTrack.clientWidth;
+			this.carouselTrack.scrollLeft = left;
 		}
 	}
 
-	prev() {
-		if (typeof this.carouselTrack.scrollTo === 'function') {
-			this.carouselTrack.scrollTo({
-				top: 0,
-				left: this.carouselTrack.scrollLeft -= this.carouselTrack.clientWidth,
-				behavior: 'smooth'
-			});
-		} else {
-			this.carouselTrack.scrollLeft -= this.carouselTrack.clientWidth;
+	createPagination() {
+		const fullScrollCount = Math.round(this.carouselTrack.scrollWidth / this.carousel.clientWidth);
+		this.pagination = document.createElement('div');
+		this.pagination.className = 'pagination';
+
+		for (let i = 0; i < fullScrollCount; i++) {
+			const page = document.createElement('button');
+			page.className = 'page';
+			page.setAttribute('data-page', i);
+			page.tabIndex = -1;
+			this.pagination.appendChild(page);
 		}
+
+		this.carousel.appendChild(this.pagination);
+		this.pagination.onclick = this.handlePageClick.bind(this);
+	}
+
+	setActivePagination() {
+		const currentPage = Math.round(this.carouselTrack.scrollLeft / this.carousel.clientWidth);
+		this.pagination.childNodes[this.currentPage].classList.remove('_current');
+		this.pagination.childNodes[currentPage].classList.add('_current');
+		this.currentPage = currentPage;
+	}
+
+	handlePageClick(event) {
+		event.preventDefault();
+		const eventTarget = event.target;
+		let pageIndex = eventTarget.getAttribute('data-page');
+		if (!pageIndex) {
+			return;
+		}
+		this.scrollTo(Math.round(this.carousel.clientWidth * pageIndex));
+	}
+
+	destroy() {
+		this.carouselTrack.removeEventListener('scroll', this.onScroll);
+		this.prevButton.removeEventListener('click', this.prev);
+		this.nextButton.removeEventListener('click', this.next);
+		this.pagination.delete();
 	}
 }
 
